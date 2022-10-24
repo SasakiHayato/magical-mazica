4915955bbcc643ed96166fe7a25490672b6d7ffe
tetora ikarashi <teto670013@gmail.com>
2022-10-24 14:40:22 +0900

    Map生成の変更

--- Assets/Member/tetora/Scripts/CreateMap.cs
+++ Assets/Member/tetora/Scripts/CreateMap.cs
@@ -13,13 +13,13 @@ public class CreateMap : MonoBehaviour
     [SerializeField]
     FieldScriptableObject _scriptableObject;
     [SerializeField]
-    GameObject[] _mapChip;//0:壁,1:横の道,2:
+    GameObject _wallObj;
     [SerializeField]
     GameObject _parentObj;
     [SerializeField]
     int _mapScale = 14;
     [SerializeField]
-    float _tileSize = 3;
+    float _tileSize = 3;//マップ一つ一つのサイズ
     Map[,] _map;
     int _startDigPos;//掘り始める始点
     bool _isCreate = default;//生成
@@ -123,7 +123,7 @@ public class CreateMap : MonoBehaviour
             {
                 continue;
             }
-            var wall = Instantiate(_mapChip[0]);//
+            var wall = Instantiate(_wallObj);//
             wall.transform.SetParent(_parentObj.transform);
             wall.transform.position = new Vector2(pos.Id % _scriptableObject.MapHorSide - _scriptableObject.MapHorSide / 2,
                 pos.Id / _scriptableObject.MapHorSide  - _scriptableObject.MapVerSide / 2 ) * _tileSize;
@@ -132,6 +132,7 @@ public class CreateMap : MonoBehaviour
     void CreateEnemy()
     {
         List<Map> generatablePosList = new List<Map>();
+
         foreach (var floar in _map)
         {
             if (floar.State == MapState.Floar)//Floarの場所を保存
