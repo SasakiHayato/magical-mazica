2c5c4dae06fce07300af22a14322833d487e9c52
DESKTOP-9OQDV70\Calaxia Akato <chiisanaaki@gmail.com>
2022-10-31 20:17:46 +0900

    Cinemachine導入

---
+++ Assets/Member/Akimoto/Scripts/CameraController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+
+public class CameraController : MonoBehaviour
+{
+
+    public void Setup()
+    {
+
+    }
+}
