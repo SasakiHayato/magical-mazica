8fd8bb49a9ba207014e945a41e1b4e49a650ad87
DESKTOP-9OQDV70\Calaxia Akato <chiisanaaki@gmail.com>
2022-10-31 21:54:33 +0900

    カメラ制御クラスの作成

--- Assets/Member/Akimoto/Scripts/CameraController.cs
+++ Assets/Member/Akimoto/Scripts/CameraController.cs
@@ -7,9 +7,17 @@ using DG.Tweening;
 
 public class CameraController : MonoBehaviour
 {
+    [SerializeField] Transform _lookAt;
 
-    public void Setup()
+    //public void Setup()
+    //{
+    //}
+
+    public Vector2 SetPosition
     {
-
+        set
+        {
+            _lookAt.localPosition = value;
+        }
     }
 }
