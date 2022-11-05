de4804da9093b9c9df2741df90314deabad415c7
SasakiHayato0605 <zuomusundou685@gmail.com>
2022-11-06 03:48:06 +0900

    いらないものの削除。Folderの移動。PoolSystemの追加

---
+++ Assets/Global/Scripts/ObjectPool/Scripts/IPool.cs
@@ -0,0 +1,25 @@
+namespace ObjectPool
+{
+    /// <summary>
+    /// PoolさせるObjectに継承
+    /// </summary>
+    public interface IPool
+    {
+        /// <summary>
+        /// Start関数
+        /// </summary>
+        /// <param name="parent">Poolの親</param>
+        void Setup(UnityEngine.Transform parent);
+
+        /// <summary>
+        /// OnEnable関数
+        /// </summary>
+        void OnEnableEvent();
+
+        /// <summary>
+        /// 使用中のUpdate関数
+        /// </summary>
+        /// <returns>使用終了時にTureを返す</returns>
+        bool Execute();
+    }
+}
