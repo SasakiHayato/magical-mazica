cd6568c78c7e10ca72866049a9510b45ce6ad97a
SasakiHayato0605 <zuomusundou685@gmail.com>
2022-11-19 17:49:28 +0900

    Enemyの作成

--- Assets/Member/Sasaki/Scripts/Enemy/Foundation/ApproachEnemy.cs
+++ Assets/Member/Sasaki/Scripts/Enemy/Foundation/ApproachEnemy.cs
@@ -3,9 +3,12 @@ using UnityEngine;
 public class ApproachEnemy : EnemyBase
 {
     [SerializeField] EnemyAIData _enemyAIData;
+    FieldTouchOperator _fieldTouchOperator;
 
     protected override void Setup()
     {
+        _fieldTouchOperator = GetComponentInChildren<FieldTouchOperator>();
+
         MonoState
             .AddState(State.Idle, new EnemyStateIdle())
             .AddState(State.Move, new EnemyStateMove())
@@ -18,6 +21,12 @@ public class ApproachEnemy : EnemyBase
 
     protected override void Execute()
     {
+        if (_fieldTouchOperator.IsTouch(FieldTouchOperator.TouchType.Wall)
+            || !_fieldTouchOperator.IsTouch(FieldTouchOperator.TouchType.Ground))
+        {
+            transform.localScale *= new Vector2(-1, 1);
+        }
+
         Vector2 move = EnemyStateData.MoveDir * Speed;
         move.y = Physics2D.gravity.y;
         RB.velocity = move;
