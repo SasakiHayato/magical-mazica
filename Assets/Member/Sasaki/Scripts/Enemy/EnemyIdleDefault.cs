e3df274cae6b682ce54d1da10ec5b87bf64c7b8d
SasakiHayato0605 <zuomusundou685@gmail.com>
2022-11-20 04:05:29 +0900

    Folderの移動、サウンドデータの追加。

--- Assets/Member/Sasaki/Scripts/Enemy/EnemyIdleDefault.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class EnemyIdleDefault : IEnemyIdle
-{
-    public void Setup(Transform user) { }
-    public Vector2 OnMove() => Vector2.zero;
-    public void Initalize() { }   
-}
