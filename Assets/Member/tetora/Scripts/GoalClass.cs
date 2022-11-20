f42ea6d76039b6f56cc28e43c946e76a54bcfc42
SasakiHayato0605 <zuomusundou685@gmail.com>
2022-11-20 10:26:39 +0900

    ゴールした際の処理を追加

--- Assets/Member/tetora/Scripts/GoalClass.cs
+++ Assets/Member/tetora/Scripts/GoalClass.cs
@@ -2,17 +2,68 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GoalClass : MonoBehaviour
+public class GoalClass : MonoBehaviour, IUIOperateEventable
 {
     [SerializeField]
     float _goalDis = 1.0f;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    int _currentSelectID;
+
+    readonly int MaxSelectID = 2;
+
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && GameController.Instance.UserInput.IsOperateRequest)
         {
             Debug.Log("ゴールした");
             //マップを移動する関数をここで呼ぶ
+            GameController.Instance.UserInput.ChangeInput(UserInputManager.InputType.UserInterface);
+            GameController.Instance.UserInput.OperateRequest(this);
         }
+    }
+
+    void IUIOperateEventable.OnEnableEvent()
+    {
+        _currentSelectID = 0;
+    }
+
+    void IUIOperateEventable.Select(ref int horizontal, ref int vertical)
+    {
+        _currentSelectID = horizontal;
+
+        if(0 < _currentSelectID)
+        {
+            _currentSelectID = 0;
+        }
+
+        if (_currentSelectID >= MaxSelectID)
+        {
+            _currentSelectID = MaxSelectID - 1;
+        }
+
+        horizontal = _currentSelectID;
+    }
+
+    bool IUIOperateEventable.SubmitEvent()
+    {
+        if (_currentSelectID == 1)
+        {
+            GameController.Instance.Dispose();
+            GameController.Instance.Setup();
+        }
+
+        return true;
+    }
+
+    void IUIOperateEventable.CancelEvent()
+    {
+        GameController.Instance.UserInput.ChangeInput(UserInputManager.InputType.Player);
+        GameController.Instance.UserInput.OperateRequest(this);
+    }
+
+    void IUIOperateEventable.DisposeEvent()
+    {
+        GameController.Instance.UserInput.ChangeInput(UserInputManager.InputType.Player);
+        GameController.Instance.UserInput.OperateRequest(this);
     }
 }
