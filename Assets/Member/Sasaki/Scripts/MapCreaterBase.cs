892bdda4e9d33f7de35b9ccbae710560c227fc75
SasakiHayato0605 <zuomusundou685@gmail.com>
2022-11-21 19:17:16 +0900

    Map生成の基底クラスを定義

---
+++ Assets/Member/Sasaki/Scripts/MapCreaterBase.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public abstract class MapCreaterBase : MonoBehaviour, IGameSetupable, IGameDisposable
+{
+    int IGameSetupable.Priority => 3;
+    public abstract Transform PlayerTransform { get; protected set; }
+
+    void Awake()
+    {
+        GameController.Instance.AddGameSetupable(this);
+        GameController.Instance.AddGameDisposable(this);
+    }
+
+    void IGameSetupable.GameSetup()
+    {
+        Create();
+    }
+
+    void IGameDisposable.GameDispose()
+    {
+        Initalize();
+    }
+
+    protected abstract void Create();
+    protected abstract void Initalize();
+}
