684410d3e8b32262345a7380631db50863af2e04
tetora ikarashi <teto670013@gmail.com>
2022-12-06 19:25:02 +0900

    Boss部屋でのPlayer、Bossの生成位置の追加

--- Assets/Member/tetora/Scripts/CreateBossStage.cs
+++ Assets/Member/tetora/Scripts/CreateBossStage.cs
@@ -11,6 +11,10 @@ public class CreateBossStage : MapCreaterBase
     [SerializeField]
     Transform _createMapPos;//生成する場所
     [SerializeField]
+    Transform _playerPos;
+    [SerializeField]
+    Transform _bossPos;
+    [SerializeField]
     int _moveSize = 18;
 
     int _dataNum;//何個データ作ったか
@@ -67,6 +71,27 @@ public class CreateBossStage : MapCreaterBase
             _createdNum++;
             SetMapTip(stage);
         }
+    }
+    /// <summary>
+    /// Tagのついたオブジェクトの場所をセットする
+    /// </summary>
+    /// <param name="objTag">オブジェクトのタグ</param>
+    /// <returns>Transform</returns>
+    public Transform SetCharaPos(string objTag)
+    {
+        if (string.IsNullOrEmpty(objTag))
+        {
+            return null;
+        }
+        if (objTag == "Player")
+        {
+            return _playerPos;
+        }
+        if (objTag == "Boss")
+        {
+            return _bossPos;
+        }
+        return null;
     }
     /// <summary>
     /// ステージの場所変更
