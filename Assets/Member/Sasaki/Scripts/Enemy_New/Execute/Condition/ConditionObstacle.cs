3dc23a32ae5e6f9265e9287934528a204ace27ae
SasakiHayato0605 <zuomusundou685@gmail.com>
2022-12-16 12:47:24 +0900

    攻撃Hit時のノックバックとEffectの追加

---
+++ Assets/Member/Sasaki/Scripts/Enemy_New/Execute/Condition/ConditionObstacle.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using BehaviourTree.Execute;
+
+public class ConditionObstacle : BehaviourConditional
+{
+    [SerializeField] LayerMask _hitLayer;
+
+    protected override bool Try()
+    {
+        if (GameController.Instance.Player == null) return false;
+      
+        return !IsHit();
+    }
+
+    bool IsHit()
+    {
+        float distance = Vector2.Distance(User.transform.position, GameController.Instance.Player.position);
+        return Physics2D.Raycast(User.transform.position, GameController.Instance.Player.position, distance, _hitLayer);
+    }
+}
