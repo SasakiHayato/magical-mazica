c70331fbcac5281d83d4e3451b6c188371a7234a
tetora ikarashi <teto670013@gmail.com>
2022-12-28 14:26:29 +0900

    tutorial用のクラス作成

---
+++ Assets/Member/tetora/Scripts/TutorialScripts/TutorialUIManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UIManagement;
+
+public class TutorialUIManager : MonoBehaviour, IGameSetupable
+{
+    [SerializeField] TutorialFieldManager _fieldManager;
+    [SerializeField] CharacterManager _characterManager;
+    [SerializeField] RawMaterialData _materialData;
+    [SerializeField] PlayerStatusPanel _playerStatusPanel;
+    [SerializeField] List<Popup> _popupList;
+
+    int IGameSetupable.Priority => 2;
+
+    static List<Popup> s_popupList;
+
+    void Awake()
+    {
+        s_popupList = _popupList;
+        GameController.Instance.AddGameSetupable(this);
+    }
+
+    void IGameSetupable.GameSetup()
+    {
+        //画面に表示する素材画像を設定
+        if (_fieldManager)
+        {
+            _fieldManager.MaterialList
+            .Subscribe(items =>
+            {
+                List<RawMaterialDatabase> materialDatas = new List<RawMaterialDatabase>();
+                items.ForEach(item => materialDatas.Add(_materialData.GetMaterialData(item)));
+                _playerStatusPanel.SetMaterialSprite(materialDatas);
+            })
+            .AddTo(_fieldManager);
+        }
+
+        //Playerが生成されたらPlayerの情報をPlayerのステータスを表示するクラスに渡す
+        if (_characterManager)
+        {
+            _characterManager.PlayerSpawn
+            .Subscribe(p =>
+            {
+                _playerStatusPanel.SetSlider(p);
+                _playerStatusPanel.SetMaterialViewPanel(p);
+            })
+            .AddTo(_characterManager);
+        }
+    }
+
+    public static Popup FindPopup(string path)
+    {
+        return s_popupList.Find(p => p.Path == path);
+    }
+}
